5931978ddf680cf19b10ef1081a81601479eedde
Siju Xminds <siju@datazoom.io>
2023-04-11 13:52:07 +0530

    first commit

---
+++ AddANumber.java
@@ -0,0 +1,17 @@
+public class AddANumber {
+    int a;
+    int b;
+    public static void AddANumber(int a, int b){
+        int tot=a+b;
+        System.out.println(tot);
+    }
+    public static void main (String args[]){
+        AddANumber(2,5);
+        AddANumber addANumber=new AddANumber();
+        addANumber(3,4);
+    }
+
+    private static int addANumber(int a, int c) {
+        return a+c;
+    }
+}
